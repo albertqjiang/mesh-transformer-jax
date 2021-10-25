94e2e4c8a3e3fa3a098e1e0e2661f950ea27f7d4
Albert Jiang on TPU <albert594250@gmail.com>
2021-10-25 18:00:52 +0000

    saving all checkpoints

--- device_train.py
+++ device_train.py
@@ -47,7 +47,7 @@ def parse_args():
     return args
 
 
-def save(network, step, bucket, path, mp, aux=None, keep_n=3, delete_old=True):
+def save(network, step, bucket, path, mp, aux=None, keep_n=20, delete_old=True):
     assert path
     client = storage.Client()
 
